43f433a6a2ba30008a11af71edd962ef520fc4a0
munisystem <ariause@gmail.com>
2014-08-05 22:00:10 +0900

    update

--- hanayo.php
+++ hanayo.php
@@ -17,7 +17,7 @@ echo "Welcome to hanayo.php!!" . PHP_EOL
 while (1){
 	//入力待ち
 	$standby = trim(fgets(STDIN));
-       	switch($standby){
+	switch($standby){
 	case "":
 		//検索単語 取得件数 言語を配列に
 		$search_options = array(
@@ -35,8 +35,8 @@ while (1){
 		$decode_obj = json_decode($search_obj, true);
 		//UTF-8 -> SJIS Mac,Linux環境ならコメントアウト推奨
 		mb_convert_variables('sjis', 'utf-8', $decode_obj);
-                //検索結果の件数をcount
-        	$result_number = count($decode_obj['statuses']);
+		//検索結果の件数をcount
+		$result_number = count($decode_obj['statuses']);
 		//timezoneの設定
 		date_default_timezone_set('Asia/Tokyo');
 		//検索結果の中で一番投稿時間の遅いものを取得
@@ -47,36 +47,36 @@ while (1){
 		$per_minutes = $result_number * 60 / (time() - $timestamp_last);
 		//おこめ per minutes投稿用ワード
 		$per_minutes_word = mb_convert_encoding("現在 $per_minutes [okome/min]",'utf-8', 'sjis'); 
-		
+
 		//かよちんにごはんをあげよう
 		if($per_minutes >= $config['best']['score']){
-		$tweet_word = $per_minutes_word . $config['best']['message'];
-		$hanayo_review = $connection->OAuthRequest(
-        		'https://api.twitter.com/1.1/statuses/update.json',
-			'POST',
-			array("status"=>$tweet_word)
-		);
+			$tweet_word = $per_minutes_word . $config['best']['message'];
+			$hanayo_review = $connection->OAuthRequest(
+				'https://api.twitter.com/1.1/statuses/update.json',
+				'POST',
+				array("status"=>$tweet_word)
+			);
 		}elseif($per_minutes >= $config['better']['score']){ 
-		$tweet_word = $per_minutes_word . $config['better']['message']; 	
-		$hanayo_review = $connection->OAuthRequest(
-        		'https://api.twitter.com/1.1/statuses/update.json',
-			'POST',
-			array("status"=>$tweet_word)
-		);
+			$tweet_word = $per_minutes_word . $config['better']['message']; 	
+			$hanayo_review = $connection->OAuthRequest(
+				'https://api.twitter.com/1.1/statuses/update.json',
+				'POST',
+				array("status"=>$tweet_word)
+			);
 		}elseif($per_minutes >= $config['worse']['score']){
-		$tweet_word = $per_minutes_word . $config['worse']['message'];        	
-		$hanayo_review = $connection->OAuthRequest(
-        		'https://api.twitter.com/1.1/statuses/update.json',
-			'POST',
-			array("status"=>$tweet_word)
-		);
+			$tweet_word = $per_minutes_word . $config['worse']['message'];        	
+			$hanayo_review = $connection->OAuthRequest(
+				'https://api.twitter.com/1.1/statuses/update.json',
+				'POST',
+				array("status"=>$tweet_word)
+			);
 		}else{
-                $tweet_word = $per_minutes_word . $config['worst']['message']; 
-		$hanayo_review = $connection->OAuthRequest(
-        		'https://api.twitter.com/1.1/statuses/update.json',
-			'POST',
-			array("status"=>$tweet_word)
-		);
+			$tweet_word = $per_minutes_word . $config['worst']['message']; 
+			$hanayo_review = $connection->OAuthRequest(
+				'https://api.twitter.com/1.1/statuses/update.json',
+				'POST',
+				array("status"=>$tweet_word)
+			);
 		}
 		echo "tweet end...";	
 		exit;	
